76a4d0e0654cacc1afe5f172e68ef4497c3d5134
yuka1007 <yuka1007@hotmail.co.jp>
2021-08-25 13:48:04 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("api.coin.z.com/public/v1/ticker")
+      .then((response) => (this.info = response));
+  },
+};
+</script>
